76142ecc557661585337a22018d48c86fd852c8b
Harshal Gharat <106591004+harshal019@users.noreply.github.com>
2026-01-30 19:41:15 +0530

    Update install-jenkins.sh

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -11,7 +11,7 @@ sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update
+sudo apt update -y
 sudo apt install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl status jenkins
